ffcae70474245d24ee815d3654c6ddc150f7e111
Matheus Martinez <matheus.martinez@ctw.bmwgroup.com>
2022-08-25 01:15:08 +0100

    Fix responsive issue

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { contestColors } from "./theme/colors";
 import { api } from "./utils/api";
 import { AxiosError } from "axios";
 import { format, parseISO } from "date-fns";
+import { styled } from "@stitches/react";
 
 function App() {
   const [lotteries, setLotteries] = useState<Lottery[]>([]);
@@ -106,100 +107,128 @@ function App() {
     getContestById();
   }, [selectedContest]);
 
+  const Wrapper = styled("div", {
+    display: "flex",
+    flexDirection: "row",
+    "@media only screen and (max-width: 1023px)": {
+      flexDirection: "column",
+    },
+  });
+
+  const LotteryContainer = styled("div", {
+    width: "40%",
+    height: "100vh",
+    paddingLeft: "96px",
+    minWidth: "400px",
+    minHeight: "40vh",
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: getBackgroundColor(),
+    justifyContent: "space-around",
+    "@media only screen and (max-width: 1023px)": {
+      height: "30vh",
+      width: "100%",
+      justifyContent: "space-around",
+      minWidth: "875px",
+      alignItems: "center",
+      paddingLeft: "0px",
+    },
+    "@media only screen and (max-width: 872px)": {
+      height: "100vh",
+    },
+  });
+
+  const SortedNumbersContainer = styled("div", {
+    height: "100vh",
+    display: selectedContest ? "flex" : "none",
+    flexDirection: "column",
+    backgroundColor: "#EFEFEF",
+    alignItems: "center",
+    width: "60%",
+    justifyContent: "space-around",
+    "@media only screen and (max-width: 1023px)": {
+      flexDirection: "column",
+      height: "70vh",
+      minHeight: "70vh",
+      minWidth: "869px",
+      width: "100%",
+    },
+    "@media only screen and (max-width: 872px)": {
+      paddingTop: "50px",
+      height: "100vh",
+    },
+  });
+
+  const LotteryFooter = styled("div", {
+    display: "flex",
+    flexDirection: "column",
+    position: "relative",
+    gap: 15,
+    "@media only screen and (max-width: 1023px)": {
+      alignItems: "center",
+    },
+  });
+
   return (
-    <div>
-      <div
-        style={{
-          width: "100%",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "row",
-          backgroundColor: getBackgroundColor(),
-        }}
-      >
+    <Wrapper>
+      <LotteryContainer>
+        <Select
+          onValueChange={handleOnSelectValueChange}
+          lotteries={lotteries}
+          label="Concuros"
+          labelPlaceHolder="Selecione o concurso"
+        />
         <div
           style={{
-            width: "40%",
-            paddingLeft: "96px",
-            minWidth: "400px",
             display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-around",
-          }}
-        >
-          <Select
-            onValueChange={handleOnSelectValueChange}
-            lotteries={lotteries}
-            label="Concuros"
-            labelPlaceHolder="Selecione o concurso"
-          />
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "row",
-              gap: 20,
-            }}
-          >
-            <img width={"60px"} height={"56px"} src={Logo} alt="Logo Mega" />
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                fontSize: "30px",
-                color: "#ffffff",
-                fontWeight: 700,
-              }}
-            >
-              {selectedContest.toUpperCase()}
-            </p>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              position: "relative",
-              gap: 15,
-            }}
-          >
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                fontSize: "14px",
-                color: "#ffffff",
-                fontWeight: 500,
-                letterSpacing: "0.135em",
-                lineHeight: "17px",
-              }}
-            >
-              {selectedContest && "CONCURSO"}
-            </p>
-            <p
-              style={{
-                fontFamily: "Montserrat",
-                fontSize: "20px",
-                color: "#ffffff",
-                fontWeight: 700,
-                lineHeight: "17px",
-              }}
-            >
-              {selectedContest &&
-                `${contestData.id} - ${contestData.dataFormatada}`}
-            </p>
-          </div>
-        </div>
-        <div
-          style={{
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            backgroundColor: "#EFEFEF",
             alignItems: "center",
-            width: "100%",
-            justifyContent: "space-around",
+            flexDirection: "row",
+            gap: 20,
           }}
         >
-          <div style={{ display: "hidden", marginTop: "20px" }} />
-          <SortedNumberList numbers={contestData?.numeros} />
+          <img width={"60px"} height={"56px"} src={Logo} alt="Logo Mega" />
+          <p
+            style={{
+              fontFamily: "Montserrat",
+              fontSize: "30px",
+              color: "#ffffff",
+              fontWeight: 700,
+            }}
+          >
+            {selectedContest.toUpperCase()}
+          </p>
+        </div>
+        <LotteryFooter>
+          <p
+            style={{
+              fontFamily: "Montserrat",
+              fontSize: "14px",
+              color: "#ffffff",
+              fontWeight: 500,
+              letterSpacing: "0.135em",
+              lineHeight: "17px",
+            }}
+          >
+            {selectedContest && "CONCURSO"}
+          </p>
+          <p
+            style={{
+              fontFamily: "Montserrat",
+              fontSize: "20px",
+              color: "#ffffff",
+              fontWeight: 700,
+              lineHeight: "17px",
+            }}
+          >
+            {selectedContest &&
+              `${contestData.id} - ${contestData.dataFormatada}`}
+          </p>
+        </LotteryFooter>
+      </LotteryContainer>
+      <SortedNumbersContainer>
+        <div style={{ display: "hidden", marginTop: "40px" }} />
+        <SortedNumberList numbers={contestData?.numeros} />
+        <div style={{ marginTop: "3rem" }}>
           <p
             style={{
               fontFamily: "Montserrat",
@@ -213,8 +242,8 @@ function App() {
             com a CAIXA.
           </p>
         </div>
-      </div>
-    </div>
+      </SortedNumbersContainer>
+    </Wrapper>
   );
 }
 
